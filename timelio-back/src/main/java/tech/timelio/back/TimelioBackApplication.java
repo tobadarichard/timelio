94321164d5296de77bdca46712fa50216ac1aa5d
Richard <richardotoba@gmail.com>
2021-07-14 20:23:05 +0100

    Creation du projet pour le back-end

---
+++ timelio-back/src/main/java/tech/timelio/back/TimelioBackApplication.java
@@ -0,0 +1,13 @@
+package tech.timelio.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelioBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelioBackApplication.class, args);
+	}
+
+}
---
+++ timelio-back/src/test/java/tech/timelio/back/TimelioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.timelio.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelioBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
